ff7c32eeb920b59e5213513c2f9aaaec82c5f215
Francisco Vazquez <fjvlutrillo@gmail.com>
2025-06-05 23:54:41 -0600

    Removed duplicated folder wedding-site

--- wedding-site/app/admin/budget/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Presupuest() {
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <h1 className="text-3xl font-bold text-gray-600">🚧 Próximamente 🚧</h1>
-        </div>
-    );
-  }
