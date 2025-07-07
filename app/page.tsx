1e57d316fee3099539c04ea5e488b1a31ad893a2
Francisco Vazquez <fjvlutrillo@gmail.com>
2025-07-07 09:43:45 -0600

    Updated event details 3

--- app/page.tsx
+++ app/page.tsx
@@ -240,6 +240,7 @@ export default function Home() {
         <div className="text-lg sm:text-xl text-[#173039] font-bodoni mb-4">
           {/*<p className="">Ceremonia y recepción</p>*/}
           <p>16 de agosto de 2025 · 13:30 Horas</p>
+          <p></p>
           <p>Código de vestimenta</p>
           <p className="text-lg sm:text-xl mb-6 font-bodoni italic text-[#173039]">
             <span className="font-bold text-[#173039]">Cocktail</span>.
