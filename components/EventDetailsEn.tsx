1552556bf1745b91701a2f5b52b668862741e1fa
Francisco Vazquez <fjvlutrillo@gmail.com>
2026-05-11 20:11:20 -0600

    Updated event details

--- components/EventDetailsEn.tsx
+++ components/EventDetailsEn.tsx
@@ -26,7 +26,7 @@ export default function EventDetailsEN() {
             address: 'Tecamachalco, Bosques Amaluca, Puebla',
             mapUrl: 'https://maps.google.com/?q=Hacienda+San+Juan+Bautista+Amalucan',
             icon: 'M10 9h4 M12 7v5 M14 21v-3a2 2 0 0 0-4 0v3 m18 9 3.52 2.147a1 1 0 0 1 .48.854V19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-6.999a1 1 0 0 1 .48-.854L6 9 M6 21V7a1 1 0 0 1 .376-.782l5-3.999a1 1 0 0 1 1.249.001l5 4A1 1 0 0 1 18 7v14',
-            description: 'The religious ceremony will take place in the hacienda chapel. Begins at 1:00PM'
+            description: 'The religious ceremony will take place in the hacienda chapel. Begins at 2:00PM'
         },
         reception: {
             title: 'Reception',
